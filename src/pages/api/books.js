748bad8f42710f1eae0ce355e39052bc7e714ff7
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-02-12 21:45:36 +0500

    create GET and POST API

--- src/pages/api/books.js
+++ src/pages/api/books.js
@@ -1,13 +1,30 @@
 import fs from 'fs'
 import path from 'path'
 
+function getData() {
+    const filePath = path.join(process.cwd(), 'data', 'books.json')
+    const fileData = fs.readFileSync(filePath)
+    const data = JSON.parse(fileData)
+    return data
+}
+
 function handler(req, res) {
     if(req.method === 'GET') {
-        const filePath = path.join(process.cwd(), 'data', 'books.json')
-        const fileData = fs.readFileSync(filePath)
-        const data = JSON.parse(fileData)
+        const data = getData()
         console.log(data)
         return res.status(200).json({ message: data })
+    }
+    else if(req.method === 'POST') {
+        const {name, description, imgUrl} = req.body
+        const newBook = {
+            id: Date.now(),
+            name,
+            description,
+            imgUrl
+        }
+        const data = getData()
+        data.push(newBook)
+        return res.status(201).json({ message: 'New book added.', book: newBook });
     }
 }
 
