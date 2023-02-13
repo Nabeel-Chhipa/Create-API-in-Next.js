f05c7c0f475a99d104583d76f8c91be69409da7d
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-02-13 23:13:43 +0500

    connect with mongodb and add books data in database.

--- src/pages/api/books.js
+++ src/pages/api/books.js
@@ -1,18 +1,26 @@
-import fs from 'fs'
-import path from 'path'
+// import fs from 'fs'
+// import path from 'path'
+import mongodb, { MongoClient } from 'mongodb'
 
-function getData() {
-    const filePath = path.join(process.cwd(), 'data', 'books.json')
-    const fileData = fs.readFileSync(filePath)
-    const data = JSON.parse(fileData)
-    return data
-}
+// function getData() {
+//     const filePath = path.join(process.cwd(), 'data', 'books.json')
+//     const fileData = fs.readFileSync(filePath)
+//     const data = JSON.parse(fileData)
+//     return data
+// }
 
-function handler(req, res) {
+async function handler(req, res) {
+
+    const client = await MongoClient.connect('mongodb+srv://admin:admin1234@cluster0.zzb8yrf.mongodb.net/?retryWrites=true&w=majority')
+    const db = client.db('books')
+
     if(req.method === 'GET') {
-        const data = getData()
-        console.log(data)
-        return res.status(200).json({ message: data })
+        // const data = getData()
+        const books = await db.collection('books').find().sort().toArray()
+        if(!books) {
+            return res.status(500).json({message: 'Internal Server Error'})
+        }
+        return res.status(200).json({ message: books })
     }
     else if(req.method === 'POST') {
         const {name, description, imgUrl} = req.body
@@ -20,12 +28,13 @@ function handler(req, res) {
             name,
             description,
             imgUrl,
-            id: Date.now()
+            // id: Date.now()
         }
-        const data = getData()
-        data.push(newBook)
-        const filePath = path.join(process.cwd(), 'data', 'books.json')
-        fs.writeFileSync(filePath, JSON.stringify(data))
+        const generatedBook = await db.collection('books').insertOne(newBook)
+        // const data = getData()
+        // data.push(newBook)
+        // const filePath = path.join(process.cwd(), 'data', 'books.json')
+        // fs.writeFileSync(filePath, JSON.stringify(data))
         return res.status(201).json({ message: 'New book added.', book: newBook });
     }
 }
